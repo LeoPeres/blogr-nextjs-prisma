fd17b95cd35e9bfd209e3d0eb21e2866f4cddf48
Leonardo Peres <leo_ps.12@hotmail.com>
2021-10-16 13:31:44 -0300

    ♻️ Update file type

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+let prisma : PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global?.prisma) {
-    global?.prisma = new PrismaClient();
+    global.prisma = new PrismaClient();
   }
   prisma = global?.prisma;
 }
